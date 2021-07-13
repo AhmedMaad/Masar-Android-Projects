4d077dacea6102cc2beee7cd767a8c0d579e00d8
AhmedMaad <atef.ahmed1341@gmail.com>
2021-07-14 00:08:17 +0200

    Finish App

---
+++ 7- Finish App/app/src/main/java/com/maad/finishapp/MainActivity.java
@@ -0,0 +1,24 @@
+package com.maad.finishapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                finish();
+            }
+        }, 5000);
+
+    }
+}
