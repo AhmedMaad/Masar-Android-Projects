243f3701ae38167a8748036a2352b15855a23056
AhmedMaad <atef.ahmed1341@gmail.com>
2021-07-14 13:39:34 +0200

    Camera App

---
+++ 9- Camera App/app/src/main/java/com/maad/cameraapp/MainActivity.java
@@ -0,0 +1,35 @@
+package com.maad.cameraapp;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void openCamera(View view) {
+        Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(i, 101);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Bundle bundle = data.getExtras();
+        Bitmap image = (Bitmap) bundle.get("data");
+        ImageView iv = findViewById(R.id.iv);
+        iv.setImageBitmap(image);
+    }
+
+}
